5baed76ab85d70a3830a68b5bcf2424c080bd81f
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-18 02:23:23 +0530

    added UC-4

---
+++ GenericMax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMaxOfN
+{
+    public class GenericMax<T> where T : IComparable
+    {
+
+
+
+        public void FindMax(T[] values)
+        {
+            Array.Sort(values);
+            Console.WriteLine(values[^1]);
+            Console.WriteLine(values[values.Length - 1]);
+            return;
+
+        }
+
+
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 
-namespace MaxProblemUsingGenerics
+namespace GenericMaxOfN
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GenericMax<int> obj1 = new GenericMax<int>();
+            int[] arr = { 122, 2323, 121, 33, 45 };
+            obj1.FindMax(arr);
 
-            MaxStrings obj1 = new MaxStrings();
-            obj1.ReturnMax("Animal", "Mongoose", "Anaconda");
+            GenericMax<double> obj2 = new GenericMax<double>();
+            double[] arr2 = { 122.3, 23.23, 121.67, 33.3, 45 };
+            obj2.FindMax(arr2);
+
+            GenericMax<string> obj3 = new GenericMax<string>();
+            string[] arr3 = { "lion", "monkey", "deer", "cobra" };
+            obj3.FindMax(arr3);
         }
     }
 }
