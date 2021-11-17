a4490e28f9f06635329947ae7db3e5fa57497ec3
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-18 01:09:30 +0530

    Add project files.

---
+++ MaxOfThree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxProblemUsingGenerics
+{
+    public class MaxOfThree
+    {
+
+        public void ReturnMax(int n1, int n2, int n3)
+        {
+            if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0 || n1.CompareTo(n2) >= 0 && n1.CompareTo(n3) > 0 || n1.CompareTo(n2) > 0 && n1.CompareTo(n3) >= 0)
+            {
+                Console.WriteLine(n1 + " is greter than " + n2 + " " + n3);
+            }
+            else if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0 || n2.CompareTo(n1) >= 0 && n2.CompareTo(n3) > 0 || n2.CompareTo(n1) > 0 && n2.CompareTo(n3) >= 0)
+            {
+                Console.WriteLine(n2 + " is greter than " + n1 + " " + n3);
+            }
+            else if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0 || n3.CompareTo(n2) >= 0 && n3.CompareTo(n1) > 0 || n3.CompareTo(n2) > 0 && n3.CompareTo(n2) >= 0)
+            {
+                Console.WriteLine(n3 + " is greter than " + n2 + " " + n1);
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaxProblemUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxOfThree obj1 = new MaxOfThree();
+            obj1.ReturnMax(25, 56, 11);
+        }
+    }
+}
